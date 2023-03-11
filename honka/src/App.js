74a7a1e9e43d88afc353d52cc11d513661bf605a
Gautam <gautammalpani33@gmail.com>
2023-03-11 19:33:11 +0530

    ExampleCardsUpdate

--- honka/src/App.js
+++ honka/src/App.js
@@ -5,14 +5,13 @@ import Home from './Components/Home';
 import Example from './pages/CardsDisplay/Example';
 // import MainImage from './Components/Navbar/MainImage';
 // import Login from './Components/Login/Login';
-import Navbar from './Components/Navbar';
+//import Navbar from './Components/Navbar';
 import Landing from './pages/Landing/Landing';
-import { Footer } from './Components/Footer';
-import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer';
 import Slider from './Components/Slider';
 import Login from './Components/Login/Login';
 import Register from './Register';
-import Profile from './components/Profile/Profile';
+import Profile from './Components/Profile/Profile';
 // import Services from './Components/Navbar/Services';
 // import Landing from  "./Components/Landing"
 // import TravelHome from './Components/TravelHome';
