f37d1349f646fdd9e21608bb590177db2169da9e
Gautam <gautammalpani33@gmail.com>
2023-03-12 03:26:52 +0530

    commit maps

--- honka/src/App.js
+++ honka/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { Route, Router, Routes } from 'react-router-dom';
 
@@ -8,45 +7,35 @@ import Example from './pages/CardsDisplay/Example';
 // import MainImage from './Components/Navbar/MainImage';
 // import Login from './Components/Login/Login';
 
-import Landing from './pages/Landing/Landing';
+// import Landing from './pages/Landing/Landing';
 import Footer  from './Components/Footer/Footer';
+
 import Navbar from './Components/Navbar/Navbar';
 import Status from './Components/Status/Status';
 import Login from './Components/Login/Login';
 import Register from './Register';
-import Profile from './components/Profile/Profile';
-import { Route, Router, Routes } from 'react-router-dom';
-import Landing from "./pages/landing"
+import Profile from './Components/Profile/Profile';
+import ProfileDisplay from "./Components/ProfileDisplay/ProfileDisplay"
+import Contact from './Components/Contact';
+import Landing from "./pages/Landing/Landing"
 
-// import Services from './Components/Navbar/Services';
-// import Landing from  "./Components/Landing"
-// import TravelHome from './Components/TravelHome';
-// import Footer from  "./Components/Footer"
-// import { Route, Router, Routes } from 'react-router-dom';
-// import Profile from './Profile/Profile';
-// import Community from './Components/Community';
-
-// import Register from './Components/Register/Register';
-
-
-// import {Route, Switch, Redirect} from "react-router-dom";
 import MumbaiMap from './Map';
 
 function App() {
   return (
     <>
     <Navbar/>
-    <MumbaiMap/>
     <Routes>
         <Route exact path="/" element={<Landing />}></Route>
         <Route exact path="/home" element={<Home />}></Route>
         <Route exact path="/status" element={<Status/>} ></Route>
-     
+        <Route exact path="/maps" element={<MumbaiMap/>} ></Route>
+    
         <Route exact path="/profile" element={<Profile />}></Route>
         <Route exact path="/example" element={<Example />}></Route>
 
-        <Route exact path="/community" element={<Community />}></Route>
-        <Route exact path="/profiledisplay" element={<ProfileDisplay />}></Route>
+        <Route exact path="/contact" element={<Contact />}></Route>
+        <Route exact path="/profiledisplay/:id" element={<ProfileDisplay />}></Route>
         <Route exact path="/login" element={<Login />}></Route>
         <Route exact path="/register" element={<Register />}></Route>
         </Routes>
