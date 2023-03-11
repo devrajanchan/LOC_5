e253dc184024f22d3a047a3e0ce2e509cb5534e2
Jemishhh <jemishpatel4545@gmail.com>
2023-03-11 12:52:17 +0530

    haash hua

--- honka/src/App.js
+++ honka/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home';
+// import MainImage from './Components/Navbar/MainImage';
+// import Login from './Components/Login/Login';
+import Navbar from './components/Navbar';
+// import Services from './Components/Navbar/Services';
+// import Landing from  "./Components/Landing"
+// import TravelHome from './Components/TravelHome';
+// import Footer from  "./Components/Footer"
+// import { Route, Router, Routes } from 'react-router-dom';
+// import Profile from './Profile/Profile';
+// import Community from './Components/Community';
+// import Recommend from './Components/Recommend';
+// import Request from './Components/Request';
+// import Login from './Login';
+// import Register from './Components/Register/Register';
+
+
+// import {Route, Switch, Redirect} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+        
+
+
+   
+
+
+  
+      {/* <Footer /> */}
+       
+    </>
+    
+     
   );
 }
 
